d41522646747d2d3382fec762aa3c221866e2688
Fabio Salvalai <fsalvalai@vaudoise.ch>
2021-03-09 07:14:58 +0100

    develop tech hints in comment

--- src/app/teams-authentication.service.ts
+++ src/app/teams-authentication.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TeamsAuthenticationService {
+  
+  // something similar to : https://github.com/OfficeDev/microsoft-teams-sample-auth-node
 
   constructor() { }
 }
